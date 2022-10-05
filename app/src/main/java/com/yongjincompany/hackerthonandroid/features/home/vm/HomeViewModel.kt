f5b30df97f86894cd2cc03dc725db541ee25d18a
최민재 <cmjcmj1230@gmail.com>
2022-10-05 21:41:09 +0900

    ⚡️:: 홈 프래크먼트 기본 뷰

--- app/src/main/java/com/yongjincompany/hackerthonandroid/features/home/vm/HomeViewModel.kt
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/home/vm/HomeViewModel.kt
@@ -1,7 +1,12 @@
 package com.yongjincompany.hackerthonandroid.features.home.vm
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.Date
 
 class HomeViewModel : ViewModel() {
-    
+
+    val currentDate = MutableLiveData<Date>()
+    val isWoman = MutableLiveData<Boolean>(true)
+
 }
