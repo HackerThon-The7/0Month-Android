44054342fb2717f205491fb614a2cc77a6849593
Leesin0222 <dragon02229@naver.com>
2022-10-05 20:59:37 +0900

    ⚡ :: appcompat version up
    1.4.2 -> 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
     implementation(Dependency.Moshi.moshi)
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     kapt(Dependency.Moshi.moshiCompiler)
