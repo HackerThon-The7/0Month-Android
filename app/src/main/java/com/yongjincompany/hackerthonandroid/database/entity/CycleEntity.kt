7dd6483aadc3934e3e7ea765d6876c70db12d848
최민재 <cmjcmj1230@gmail.com>
2022-10-06 10:29:19 +0900

    ⚡️:: Cycle Dao 추가
    모두 조회, 개별 조회, 주기 등록

--- app/src/main/java/com/yongjincompany/hackerthonandroid/database/entity/CycleEntity.kt
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/database/entity/CycleEntity.kt
@@ -3,7 +3,7 @@ package com.yongjincompany.hackerthonandroid.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "cycle_table")
 data class CycleEntity(
     @PrimaryKey
     val startDate: String,
--- app/src/main/java/com/yongjincompany/hackerthonandroid/database/entity/DayStateEntity.kt
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/database/entity/DayStateEntity.kt
@@ -3,7 +3,7 @@ package com.yongjincompany.hackerthonandroid.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "day_state_table")
 data class DayStateEntity(
     @PrimaryKey
     val date: String,
