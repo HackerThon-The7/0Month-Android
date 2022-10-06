ccab3fd7392ae0df42ed275a23f89488afd0a154
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-06 15:47:12 +0900

    📄 :: adapter 생성

---
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/mt/view/adapter/MenstrualExaminationAdapter.kt
@@ -0,0 +1,26 @@
+package com.yongjincompany.hackerthonandroid.features.mt.view.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yongjincompany.hackerthonandroid.databinding.FragmentPeriodExaminationBinding
+
+class MenstrualExaminationAdapter : RecyclerView.Adapter<MenstrualExaminationAdapter.MenstrualExamination>() {
+    class MenstrualExamination(private val binding: FragmentPeriodExaminationBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenstrualExaminationAdapter.MenstrualExamination {        TODO("Not yet implemented")
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+
+    }
+
+    override fun onBindViewHolder(holder: MenstrualExamination, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
