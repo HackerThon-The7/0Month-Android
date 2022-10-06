5f3e6450185c1c8b7447a5a5922b347a2710d82d
최민재 <cmjcmj1230@gmail.com>
2022-10-06 16:48:34 +0900

    ⚡️:: State Adapter 작성

---
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/diary/adapter/StateAdapter.kt
@@ -0,0 +1,35 @@
+package com.yongjincompany.hackerthonandroid.features.diary.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.yongjincompany.hackerthonandroid.R
+import com.yongjincompany.hackerthonandroid.databinding.ItemStateBinding
+import com.yongjincompany.hackerthonandroid.features.diary.adapter.callback.StateDiffUtilCallback
+import com.yongjincompany.hackerthonandroid.features.diary.item.State
+
+class StateAdapter : ListAdapter<State, StateAdapter.StateViewHolder>(StateDiffUtilCallback) {
+
+    inner class StateViewHolder(private val binding: ItemStateBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(state: State) {
+            binding.state = state
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StateViewHolder {
+        return StateViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_state,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: StateViewHolder, position: Int) {
+        return holder.bind(getItem(position))
+    }
+}
---
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/diary/item/State.kt
@@ -0,0 +1,6 @@
+package com.yongjincompany.hackerthonandroid.features.diary.item
+
+data class State(
+    val type: String,
+    val isChecked: Boolean = false
+)
