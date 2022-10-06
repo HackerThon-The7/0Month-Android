38441c7e55820254c6b5f42e6edd0377c6d8e29c
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-06 12:37:00 +0900

    📄 :: question 페이지 퍼플리싱

--- app/src/main/java/com/yongjincompany/hackerthonandroid/features/mt/view/fragment/PeriodExaminationFragment.kt
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/mt/view/fragment/PeriodExaminationFragment.kt
@@ -1,13 +1,25 @@
 package com.yongjincompany.hackerthonandroid.features.mt.view.fragment
 
+import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.yongjincompany.hackerthonandroid.R
 import com.yongjincompany.hackerthonandroid.base.BaseFragment
 import com.yongjincompany.hackerthonandroid.databinding.FragmentPeriodExaminationBinding
+import com.yongjincompany.hackerthonandroid.features.mt.vm.PeriodExaminationViewModel
 
 class MenstrualExaminationFragment :
     BaseFragment<FragmentPeriodExaminationBinding>(R.layout.fragment_period_examination) {
-    override fun initView() {
 
+    private val periodExaminationViewModel by activityViewModels<PeriodExaminationViewModel>()
+
+    override fun initView() {
+        binding()
+    }
+    private fun binding() {
+        binding.fragment = this
     }
 
+    fun onClickNextBtn(view: View) {
+
+    }
 }
---
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/mt/vm/PeriodExaminationViewModel.kt
@@ -0,0 +1,7 @@
+package com.yongjincompany.hackerthonandroid.features.mt.vm
+
+import androidx.lifecycle.ViewModel
+
+class PeriodExaminationViewModel: ViewModel() {
+
+}
