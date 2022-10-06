3a7f0c62d75c298b33b57eda4775eee64b48e623
Leesin0222 <dragon02229@naver.com>
2022-10-06 13:47:34 +0900

    ⚡ :: Constanst 작성

---
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/features/chat/logic/Constants.kt
@@ -0,0 +1,10 @@
+package com.yongjincompany.hackerthonandroid.features.chat.logic
+
+object Constants {
+
+    const val SEND_ID = "SEND_ID"
+    const val RECEIVE_ID = "RECEIVE_ID"
+
+    const val OPEN_GOOGLE = "구글 접속 중..."
+    const val OPEN_SEARCH = "검색 중..."
+}
