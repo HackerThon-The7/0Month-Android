1b94d470ed2892a6483f8549bcd8e16393d74b87
최민재 <cmjcmj1230@gmail.com>
2022-10-06 09:29:26 +0900

    ⚡️:: Room Database 추가

---
+++ app/src/main/java/com/yongjincompany/hackerthonandroid/common/database/RoomDatabase.kt
@@ -0,0 +1,39 @@
+package com.yongjincompany.hackerthonandroid.common.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import com.yongjincompany.hackerthonandroid.features.home.database.entity.CycleEntity
+import java.util.concurrent.Executors
+
+@Database(
+    entities = [CycleEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class RoomDatabase : androidx.room.RoomDatabase() {
+
+//    abstract fun tokenDao(): TokenDao
+
+    companion object {
+        private var instance: RoomDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): RoomDatabase? {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RoomDatabase::class.java, "hackathon_database"
+                )
+                    .setQueryCallback({ sqlQuery, bindArgs ->
+                        println("SQL Query: $sqlQuery SQL Args: $bindArgs")
+                    }, Executors.newSingleThreadExecutor())
+                        .fallbackToDestructiveMigration()
+                        .allowMainThreadQueries()
+                        .build()
+                }
+                return instance
+            }
+        }
+    }
+    
